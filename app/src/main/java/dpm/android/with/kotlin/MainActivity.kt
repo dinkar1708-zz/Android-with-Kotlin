2c6a5d1c272ccbc0e1973b053d6a415ff6accb5b
Dinakar Maurya <dinakar.maurya@dena.com>
2020-05-13 09:47:35 +0900

    Androidx migration, library update

--- app/src/main/java/dpm/android/with/kotlin/MainActivity.kt
+++ app/src/main/java/dpm/android/with/kotlin/MainActivity.kt
@@ -1,11 +1,11 @@
 package dpm.android.with.kotlin
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         nav_view.setNavigationItemSelectedListener(this)
 
-        welcome.text = "Hey i have done setup in kotlin....playing with text view...."
+        welcome.text = getString(R.string.sample_text)
     }
 
     override fun onBackPressed() {
