9748a09ad06b219d61554e41ece6d012dd508882
Dinakar Maurya <dinkar1708@gmail.com>
2020-10-06 20:14:30 +0900

    Activity life cycle

--- app/src/main/java/dpm/android/with/kotlin/lifecycle/MainActivity.kt
+++ app/src/main/java/dpm/android/with/kotlin/lifecycle/MainActivity.kt
@@ -1,13 +1,25 @@
-package dpm.android.with.kotlin
+package dpm.android.with.kotlin.lifecycle
 
+import android.content.Intent
 import android.os.Bundle
+import dpm.android.with.kotlin.R
+
+<<<<<<< Updated upstream
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+=======
+import android.util.Log
+>>>>>>> Stashed changes
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -18,6 +30,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        Log.d(TAG, "onCreate...")
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -31,7 +44,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         nav_view.setNavigationItemSelectedListener(this)
 
+<<<<<<< Updated upstream
         welcome.text = "Hey i have done setup in kotlin....playing with text view...."
+=======
+        button.setOnClickListener {
+            navigateToSecondActivity()
+        }
+    }
+
+    private fun navigateToSecondActivity() {
+        startActivity(Intent(this, SecondActivity::class.java))
+>>>>>>> Stashed changes
     }
 
     override fun onBackPressed() {
@@ -83,5 +106,40 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart...")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume...")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause...")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop...")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy...")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart...")
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
