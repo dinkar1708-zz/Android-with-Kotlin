efacbbae8840c8db87e28cac91c02b2c5ec0558e
Maurya <dinkar1708@gmail.com>
2024-03-03 04:14:20 +0900

    theme

---
+++ app/src/main/java/com/example/clean/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.clean.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Pink = Color(0xFF7D5260)
+val DarkColor = Color(0xED503535)
+val WhiteColorDark = Color(0xFFFFFFFF)
+val TextColor = Color.Black
