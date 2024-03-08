67d18ac2ec7d2d3ef0950be6811747924d9e6d4d
Maurya <dinkar1708@gmail.com>
2024-03-09 01:25:58 +0900

    added dependency and setup tear down methods

--- app/src/test/java/com/example/clean/domain/usecase/SearchRepositoryUseCaseTest.kt
+++ app/src/test/java/com/example/clean/domain/usecase/SearchRepositoryUseCaseTest.kt
@@ -1,5 +1,11 @@
 package com.example.clean.domain.usecase
 
+import com.example.clean.data.repository.user.UserRepository
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.Assertions.*
 
 import org.junit.jupiter.api.AfterEach
@@ -7,13 +13,19 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class SearchRepositoryUseCaseTest {
+    private val mockRepository: UserRepository = mockk()
+    private val testDispatcher = StandardTestDispatcher()
+    private lateinit var userRepositoryUseCase: UserRepositoryUseCase
 
     @BeforeEach
     fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        userRepositoryUseCase = UserRepositoryUseCase(mockRepository)
     }
 
     @AfterEach
     fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
