f0ec0c8cba7f7f2fb0f1dcb96a8dd78f9b802d3a
Maurya <dinkar1708@gmail.com>
2024-03-09 01:20:05 +0900

    test case file for search repository use case

---
+++ app/src/test/java/com/example/clean/domain/usecase/SearchRepositoryUseCaseTest.kt
@@ -0,0 +1,22 @@
+package com.example.clean.domain.usecase
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class SearchRepositoryUseCaseTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun searchUsers() {
+    }
+}
