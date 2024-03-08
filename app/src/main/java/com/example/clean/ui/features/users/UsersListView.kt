dbf829926d649bff71d7b93eda7021765b2e4177
Maurya <dinkar1708@gmail.com>
2024-03-09 01:07:32 +0900

    user list screen UI fix

--- app/src/main/java/com/example/clean/ui/features/users/UsersListView.kt
+++ app/src/main/java/com/example/clean/ui/features/users/UsersListView.kt
@@ -4,15 +4,22 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,23 +41,46 @@ fun UsersListView(
         }
     }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserListItem(user: User, onItemClick: (User) -> Unit) {
     Card(
-        modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.primaryContainer).padding(8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .padding(1.dp),
         shape = RoundedCornerShape(0.dp),
         onClick = { onItemClick(user) }
     ) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Text(text = user.avatarUrl)
+        Row(
+            modifier = Modifier.padding(1.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // TODO add actual image from url
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "User Avatar",
+                tint = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.size(64.dp),
+            )
+            Spacer(modifier = Modifier.width(16.dp))
             Column {
-                Text(text = user.score.toString())
-                Text(text = user.login)
+                Text(
+                    text = user.login,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+                Text(
+                    text = "Score: ${user.score}",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                )
             }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
